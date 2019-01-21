03de69a478fd52fe1740ebc05af53ce1560b6bec
brainor <brainor@qq.com>
2019-01-21 16:39:07 +0800

    增加flux和相位分析

--- plot/eigen.m
+++ plot/eigen.m
@@ -5,7 +5,7 @@
 % 和Mauel, Drift orbits in a Symmetric Magnetic Dipole, 2015 12节
 %% with N evolution
 omegapds=linspace(1,2,500);
-omegapds=linspace(1.6,1.7,200);
+% omegapds=linspace(1.6,1.7,200);
 etas=linspace(0.1,2,2000);
 [X,Y]=meshgrid(etas,omegapds);
 rhom=1/25;
@@ -31,6 +31,7 @@ for i=1:length(omegapds)
 %         end
     end
 end
+% scan eta, omega_d
 figure
 h=subplot(1,2,1);
 contour(X,Y,imag(gr),'LevelStep',1);
@@ -46,6 +47,7 @@ suptitle('extended MHD Mode with heat flux $$\frac{p_e^2}{\tilde{N}}$$','Interpr
 an=annotation('textbox','LineStyle','none','FontSize',14);
 an.Interpreter='latex';
 an.String={['$$m=' num2str(m) '$$'],['$$m_\perp=' num2str(m_perp) '$$'],['$$\rho_\star=' num2str(rhom) '$$']};
+% scan eta
 figure%只画eta
 index=find(omegapds>=1.6666,1);
 subplot(1,2,1)
@@ -93,13 +95,13 @@ h.YLabel.Interpreter='latex';h.YLabel.String='$$\frac{\omega^\star_p}{\omega_d}$
 h.Title.String='Frequency';colorbar;
 suptitle('ideal MHD Mode with heat flux $$p_e^2$$','Interpreter','latex');
 close
-%% scan m,omegapds
+%% with N, scan m,omegapds
 omegapds=linspace(1,2,500);
 omegapd0=[2,1.9,1.8,1.7,1.6,1.5];%取特定omegapd
 iy=zeros(size(omegapd0));
 eta=0;
 ms=1:80;
-[X,Y]=meshgrid(ms,omegapds);
+[X,~]=meshgrid(ms,omegapds);
 rhom=1/25;
 gamma=5/3;
 m=1;
@@ -142,3 +144,57 @@ lgd=legend(cellfun(@num2str,num2cell(omegapd0),'UniformOutput',false),'Location'
 lgd.Title.String='$$\frac{\omega_p^\star}{\omega_d}$$';lgd.Title.Interpreter='latex';
 suptitle({'dispersion relationship';['$$\eta=',num2str(eta),'$$, $$\rho_\star=' num2str(rhom) '$$']});
 print(['plot/growthrate_eta=',num2str(eta),'.png'],'-dpng');
+%% with N, 计算flux
+% Gamma_N=real(i*m*Phi*N')
+% Gamma_p=real(i*m*Phi*p*)
+omegapds=linspace(1,2,500);omegapds=linspace(1.2,3,1000);
+etas=linspace(0.4,2,200);etas=linspace(0,6,400);
+[X,Y]=meshgrid(etas,omegapds);
+rhom=1/25;
+gamma=5/3;
+m_perp=1;
+m=1;
+
+gr=nan(size(X));
+vectors=nan(size(X,1),size(X,2),3);%N, p, Phi
+for i=1:length(omegapds)
+    for j=1:length(etas)
+        omegapd=omegapds(i);eta=etas(j);
+        A=[0,1,omegapd*1/(1+eta)-1;
+            -gamma,2*gamma,omegapd-gamma;
+            0,-1/0.6/rhom^2/m_perp^2,0];
+        [V,w]=eigs(A,1,'largestimag');%分别为特征向量和特征值
+        if imag(w)>0
+            gr(i,j)=m*max(w);
+            vectors(i,j,:)=V;
+        end
+%         w=imag(eigs(A));
+%         w=sort(w);
+%         if max(w)>0 
+%             gr(i,j)=max(w);
+%         end
+    end
+end
+h=subplot(2,2,1);
+ph=pcolor(X,Y,real(1i*m*vectors(:,:,3).*conj(vectors(:,:,1))));shading interp;ph.ZData=ph.CData;
+h.XLabel.String='\eta';
+h.YLabel.Interpreter='latex';h.YLabel.String='$$\frac{\omega^\star_p}{\omega_d}$$';
+h.Title.Interpreter='latex';h.Title.String={'Particle flux';'$\Gamma_N=\Re\{im\Phi_mP_m^\star\}$'};colorbar;
+h=subplot(2,2,2);
+ph=pcolor(X,Y,real(1i*m*vectors(:,:,3).*conj(vectors(:,:,2))));shading interp;ph.ZData=ph.CData;
+h.XLabel.String='\eta';
+h.YLabel.Interpreter='latex';h.YLabel.String='$$\frac{\omega^\star_p}{\omega_d}$$';
+h.Title.Interpreter='latex';h.Title.String={'Radial pressure flux';'$\Gamma_p=\Re\{im\Phi_mp_m^\star\}$'};colorbar;
+h=subplot(2,2,3);
+test=angle(vectors(:,:,3)./vectors(:,:,1));
+test(test>0)=test(test>0)-2*pi;
+ph=pcolor(X,Y,test);shading interp;ph.ZData=ph.CData;
+h.XLabel.String='\eta';
+h.YLabel.Interpreter='latex';h.YLabel.String='$$\frac{\omega^\star_p}{\omega_d}$$';
+h.Title.String='Phase lag between $\Phi_m$, $N_m$';h.Title.Interpreter='latex';colorbar;
+h=subplot(2,2,4);
+ph=pcolor(X,Y,angle(vectors(:,:,3)./vectors(:,:,2)));shading interp;ph.ZData=ph.CData;
+h.XLabel.String='\eta';
+h.YLabel.Interpreter='latex';h.YLabel.String='$$\frac{\omega^\star_p}{\omega_d}$$';
+h.Title.String='Phase lag between $\Phi_m$, $p_m$';h.Title.Interpreter='latex';colorbar;
+% annotation('textbox','LineStyle','none','Interpreter','latex','FontSize',12,'String',{['$$m=' num2str(m) '$$'],['$$m_\perp=' num2str(m_perp) '$$'],['$$\rho_\star=' num2str(rhom) '$$']});
